dadb4d96ba66b6d75c14321a5bcb6a1227f7af75
Eda Wright <ewrong16@gmail.com>
2025-03-25 20:56:31 -0400

    nushell aliases

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -3,9 +3,6 @@
   config = {
     programs = {
 	nushell = { enable = true;
-	  # The config.nu can be anywhere you want if you like to edit your Nushell with Nu
-	  configFile.source = ./.../config.nu;
-	  # for editing directly to config.nu 
 	  extraConfig = ''
 	   let carapace_completer = {|spans|
 	   carapace $spans.0 nushell ...$spans | from json
@@ -13,16 +10,16 @@
 	   $env.config = {
 	    show_banner: false,
 	    completions: {
-	    case_sensitive: false # case-sensitive completions
-	    quick: true    # set to false to prevent auto-selecting completions
-	    partial: true    # set to false to prevent partial filling of the prompt
-	    algorithm: "fuzzy"    # prefix or fuzzy
-	    external: {
-	    # set to false to prevent nushell looking into $env.PATH to find more suggestions
-		enable: true 
-	    # set to lower can improve completion performance at the cost of omitting some options
-		max_results: 100 
-		completer: $carapace_completer # check 'carapace_completer' 
+	      case_sensitive: false # case-sensitive completions
+	      quick: true    # set to false to prevent auto-selecting completions
+	      partial: true    # set to false to prevent partial filling of the prompt
+	      algorithm: "fuzzy"    # prefix or fuzzy
+	      external: {
+	      # set to false to prevent nushell looking into $env.PATH to find more suggestions
+		  enable: true 
+	      # set to lower can improve completion performance at the cost of omitting some options
+		  max_results: 100 
+		  completer: $carapace_completer # check 'carapace_completer' 
 	      }
 	    }
 	   } 
@@ -33,9 +30,34 @@
 	   )
 	   '';
 	   shellAliases = {
-	   vi = "hx";
-	   vim = "hx";
-	   nano = "hx";
+	    vi = "nvim";
+	    vim = "nvim";
+	    nano = "nvim";
+
+	    ls="ls -la";
+
+	    #git aliases
+	    gs="git status";
+	    ga="git add";
+	    gc="git commit -m";
+	    gch="git checkout";
+
+	    #Handy list ec2 command
+	    ec2="aws ec2 describe-instances --query \"Reservations[*].Instances[*].{Instance:InstanceId,Instance:PrivateIpAddress,Name:Tags[?Key=='Name']|[0].Value}\" --filters \"Name=tag:Application,Values=*\" --output table --region=us-east-2";
+
+	    # Wright20 repo navicagations
+	    w="tmux a -t wright";
+	    api="~/wright/wright-20-api/";
+	    ams="~/wright/auction-system/";
+	    sms="~/wright/wright20-sms";
+	    app="~/wright/wright-20-app";
+	    models="~/wright/wright20-models";
+	    cas="~/wright/CAS";
+	    util="~/wright/utility-server";
+	    cms="~/wright/wright-20-cms";
+	    helpers="~/wright/wright-20-helpers";
+	    migrations="~/wright/migrations";
+	    devd="~/wright/dev-diary";
 	   };
        };  
        carapace.enable = true;
