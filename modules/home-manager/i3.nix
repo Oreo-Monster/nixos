bd19135b96c46d53a0a6211e5fe7a013904a184a
Eda Wright <ewrong16@gmail.com>
2025-03-26 23:01:56 -0400

    Generation 50 Built at 2025-03-26 23:01:53

--- modules/home-manager/i3.nix
+++ modules/home-manager/i3.nix
@@ -82,9 +82,14 @@ in {
 
     home.file.".config/i3/scripts/move-cursor-to-focused" = {
       source = pkgs.writeScript "move-cursor-to-focused" ''
-        #!/bin/sh
-        window_id=$(i3-msg -t get_tree | jq -r '.. | select(.focused? == true) | .id')
-        xdotool mousemove --window $window_id 50% 50%
+               #!/bin/sh
+               focused_window=$(xdotool getwindowfocus -f)
+        output=$(xdotool getwindowgeometry --shell $focused_window)
+        width=$(echo $output | awk '{print $4}' | cut -d "=" -f 2)
+        height=$(echo $output | awk '{print $5}' | cut -d "=" -f 2)
+        move_y=$((height / 2))
+        move_x=$((width / 2))
+        xdotool mousemove --window $focused_window $move_x $move_y
       '';
     };
 
